09236c6e8befbde0e8e5207098e8b3fbd53273ed
Yunfei Wang <Yunfei Wang>
2017-05-24 00:05:27 -0400

    first submit

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var burger = require("../models/burger.js");
+var router = express.Router();
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  burger.create([
+    "name", "devoured"
+  ], [
+    req.body.name, req.body.devoured
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+router.delete("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  cat.delete(condition, function() {
+    res.redirect("/");
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ("Inside Out");
+INSERT INTO burgers (name) VALUES ("Juicy Lucy");
+INSERT INTO burgers (name) VALUES ("Krispy Kreme");
+INSERT INTO burgers (name) VALUES ("Red Robin");
+INSERT INTO burgers (name) VALUES ("Sloppy Joe");
